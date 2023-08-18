054905507d9644ea57dada2c2ef71795d2a01179
RamilA123 <ramilna@code.edu.az>
2023-08-18 16:58:12 +0400

    get categories on admin dashboard

--- OrganiDb/OrganiDb/Areas/Admin/Controllers/CategoriesController.cs
+++ OrganiDb/OrganiDb/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OrganiDb.Models;
+using OrganiDb.Services.Interfaces;
 
 namespace OrganiDb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoriesController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
         {
-            return View();
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Category> categories = await _categoryService.GetAllAsync();
+
+            return View(categories);
         }
     }
 }
